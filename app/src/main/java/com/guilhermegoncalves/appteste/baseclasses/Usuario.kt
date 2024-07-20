6221edc3d8e1372243a6587f10ed9622b7f9a80f
User <guilhermee9@live.com>
2024-07-19 21:37:26 -0300

    Layout cadastro user
    Adicionar imagem no cadastro do usuario

--- app/src/main/java/com/guilhermegoncalves/appteste/baseclasses/Usuario.kt
+++ app/src/main/java/com/guilhermegoncalves/appteste/baseclasses/Usuario.kt
@@ -4,5 +4,6 @@ data class Usuario(
     var key: String? = null,
     var nome: String? = null,
     var email: String? = null,
-    var password: String? = null
+    var password: String? = null,
+    val imageUrl: String = ""
 )
